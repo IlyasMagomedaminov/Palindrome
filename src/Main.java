86a2057c882eccaad2afb3c64eb8cfafc5f5a7fd
IlyasMagomedaminov <imagomedaminov00@mail.ru>
2023-05-14 13:22:03 +0300

    Palindrome сделал

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,7 @@ public class Main {
         String reversed = new StringBuffer(replaced).reverse().toString();
         return reversed.equals(replaced);
     }
-
     public static void main(String[] args) {
         System.out.println(isPalindrome("Madam, I'm Adam!"));
     }
-
 }
