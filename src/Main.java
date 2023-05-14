54d934201a5236d64c53acdad6b143ee9cfd489d
IlyasMagomedaminov <imagomedaminov00@mail.ru>
2023-05-14 13:18:05 +0300

    Palindrome

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static boolean isPalindrome(String text) {
+        String replaced = text.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+        String reversed = new StringBuffer(replaced).reverse().toString();
+        return reversed.equals(replaced);
     }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("Madam, I'm Adam!"));
+    }
+
 }
